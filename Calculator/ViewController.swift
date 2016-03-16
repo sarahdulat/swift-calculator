5b031c49c8048fe00ef54d8bb8ddea33df860da8
sarahdulat <sarah.dulat@gmail.com>
2016-03-15 23:04:18 -0400

    git commit -m "Initial commit"

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("commit")
     }
 
     override func didReceiveMemoryWarning() {
