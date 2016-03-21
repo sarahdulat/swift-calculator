c3768b1ffe10b2bf5dc942abe0bed4e3359e7931
sarahdulat <sarah.dulat@gmail.com>
2016-03-20 21:28:54 -0400

    Add functions code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,19 +15,19 @@ class ViewController: UIViewController {
     
     var isFirstDigit = true
     var operand1: Double = 0
-    var operand = "="
+    var operation = "="
     
     var displayValue: Double {
         get {
-            return
-                NSNumberFormatter().numberFromString(Label.text!)!.doubleValue
+            return NSNumberFormatter().numberFromString(Label.text!)!.doubleValue
         }
         
         set {
             Label.text = String(format: "%2.0f", newValue)
             isFirstDigit = true
-            Operation = "="
+            operation = "="
         }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,18 +39,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-        @IBAction func Digit(sender: AnyObject) {
+        @IBAction func Digit(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        Label.text = isFirstDigit ? digit : Label.text! + digit
+        isFirstDigit = false
+        
         }
 
         @IBAction func Cancel(sender: AnyObject) {
+            
+            displayValue = 0
+            
         }
         
         @IBAction func Calculate(sender: AnyObject) {
+            
+            switch operation {
+                
+                case "/":displayValue = operand1 / displayValue
+                case "*":displayValue *= operand1
+                case "+":displayValue += operand1
+                case "-":displayValue = operand1 - displayValue
+            default:break
+                
+            }
+            
         }
         
-        @IBAction func Operations(sender: AnyObject) {
+        @IBAction func Operations(sender: UIButton) {
+            
+            operation = sender.currentTitle!
+            operand1 = displayValue
+            isFirstDigit = true
+            
         }
         
         
         
-    };
+    }
